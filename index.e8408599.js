fe43a252466c309389bd0d96909f646d6417af2b
Misha Siv <0Sivcov0@gmail.com>
2024-02-11 16:22:42 +0200

    make build

---
+++ index.e8408599.js
@@ -0,0 +1,3 @@
+var e=document.querySelector("#largeImg"),t=document.querySelectorAll(".list-item__link"),r=!0,l=!1,n=void 0;try{for(var i,o=t[Symbol.iterator]();!(r=(i=o.next()).done);r=!0)!function(){var t=i.value;t.addEventListener("click",function(r){r.preventDefault(),e.src=t.getAttribute("href")})}()}catch(e){l=!0,n=e}finally{try{r||null==o.return||o.return()}finally{if(l)throw n}}//# sourceMappingURL=index.e8408599.js.map
+
+//# sourceMappingURL=index.e8408599.js.map
